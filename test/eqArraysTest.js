d31e785e08ec009c7201389ff75034d9cacb986c
dialop <dianalop1998@gmail.com>
2023-08-02 19:39:22 -0400

    Eslint

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,6 @@
-const eqArrays = require('../eqArrays')
-const assertEqual = require('../assertEqual')
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
   
-  //Test Code
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+//Test Code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
