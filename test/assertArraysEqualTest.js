677de4423e9bc108b9985cef5ea0aa98a581a121
dialop <dianalop1998@gmail.com>
2023-08-01 23:17:45 -0400

    Edited Refractured File

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+// Test Code
+assertArraysEqual([1, 2, 3], [2, 4, 6]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
