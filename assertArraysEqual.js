0aa7aab6123e232353f846078a040e6d8139824d
dialop <dianalop1998@gmail.com>
2023-08-01 23:12:19 -0400

    edited refractured file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-
-    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-
-    console.log(` 🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
-
-const eqArrays = function(series1, series2) {
-  if (series1.length !== series2.length) {
-    return false;
-  }
-
-  let index = 0;
-  for (let i of series1) {
-    if (i !== series2[index]) {
-      return false;
-    }
-    index++;
-  }
-
-  return true;
-};
-
-//
-
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
@@ -39,6 +10,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [2, 4, 6]);
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
