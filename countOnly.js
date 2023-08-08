67fa482aa41e88ce2b0706d3c143254174dfaeb1
dialop <dianalop1998@gmail.com>
2023-08-07 20:06:12 -0400

    big fixed allItems

--- countOnly.js
+++ countOnly.js
@@ -9,17 +9,9 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-
-
-  
+    if (item in itemsToCount && itemsToCount[item] === true) {
+      results[item] = (results[item] || 0) + 1;
     }
-
-    
-    console.log(item);
     
   }
 
