b2b6127e578a672d77ff52aee2998f25040246df
dialop <dianalop1998@gmail.com>
2023-08-12 23:42:15 -0400

    Fixed Bug to Assess Test

--- eqObjects.js
+++ eqObjects.js
@@ -2,30 +2,25 @@ const eqArrays = require('./eqArrays');
 
 // eqObjects will return true if two unsorted objects are the same (keys and values)
 const eqObjects = function(object1, object2) {
-  const key1 = Object.keys(object1);
-  const key2 = Object.keys(object2);
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
-
-  if (key1.length !== key2.length) {
+  if (keys1.length !== keys2.length) {
     return false;
   }
 
-  if (Array.isArray(object1) && Array.isArray(object2)) {
-    if (!eqArrays(object1, object2)) {
-      return false;
-    }
-  }
-
-  for (const key of key1) {
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-    if (!(key in object2)) {
+  for (const key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
 
   return true;
 };
+
 
 module.exports = eqObjects;
